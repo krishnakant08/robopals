cf72df1aa09af5863e8582f0663917cd5701a601
Krishna Kant Hati <krishnakant08@users.noreply.github.com>
2020-05-03 20:46:22 +0530

    Updates

---
+++ precache-manifest.a95c8d6e7da5a71903cd16489765aca7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57536d6190822b850bcec7e081f1e60",
+    "url": "/robopals/index.html"
+  },
+  {
+    "revision": "91b07351681a20327808",
+    "url": "/robopals/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "8bbc72475054f430f9c3",
+    "url": "/robopals/static/css/main.70878dcc.chunk.css"
+  },
+  {
+    "revision": "91b07351681a20327808",
+    "url": "/robopals/static/js/2.e9f8442d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robopals/static/js/2.e9f8442d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbc72475054f430f9c3",
+    "url": "/robopals/static/js/main.41599d5c.chunk.js"
+  },
+  {
+    "revision": "1d48306b4a28c009311e",
+    "url": "/robopals/static/js/runtime-main.488b732b.js"
+  },
+  {
+    "revision": "341453a917369e5c5bb60262d3ae5176",
+    "url": "/robopals/static/media/United-lgZww.341453a9.woff"
+  }
+]);
